126edeac8fe1d17a71c1196c6d0f09f0025b4310
vikornienko <vikornienko76@gmail.com>
2024-10-07 22:43:58 +0500

    Third commit. New libraties and files.

--- weather.js
+++ weather.js
@@ -1,11 +1,13 @@
 #!/usr/bin/node
-import { getArgs } from './helpers/args.js'
+import { getArgs } from './helpers/args.js';
+import { printHelp } from './services/log.service.js';
 
 const initCLI = () => {
     const userArgs = getArgs(process.argv)
-    console.log(userArgs);
+    // console.log(userArgs);
     if (userArgs.h) {
         //Показать раздел помощь.
+        printHelp();
     }
     if (userArgs.s) {
         // Сохранить название города для запроса к API.
